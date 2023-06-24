fcda5c7490e7bc773a60222ee520c37b3ea72682
RexDex Warriror <rexdexwarrior@gmail.com>
2023-06-24 22:45:29 +0700

    Init

---
+++ src/constants.ts
@@ -0,0 +1,50 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  MAINNET = 888,
+  ROPSTEN = 999,
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0xCc2F30462ED1C47Dd7Fb75C81c7F4Cd721eB2A66'
+
+export const INIT_CODE_HASH = '0x832eb774220aea72a2be00d4dc89b2db0f0d5dae4b72eaadc8e1f5a385ab568e'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
